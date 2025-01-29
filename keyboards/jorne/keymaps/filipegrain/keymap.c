e9ee101a17a293a7988ee816387c729b40425d51
Filipe R Grain <32407690+filipegrain@users.noreply.github.com>
2025-01-29 20:18:10 -0300

    fix: keymap

--- keyboards/jorne/keymaps/filipegrain/keymap.c
+++ keyboards/jorne/keymaps/filipegrain/keymap.c
@@ -29,33 +29,13 @@ enum custom_keycodes {
 #define SPC_RSH MT(MOD_RSFT, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT(
-        LALT_T(KC_MPLY),         KC_GRV,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,            KC_LBRC,         RALT_T(KC_RBRC),
-        LCTL_T(KC_CAPS),         KC_A,          KC_S,           KC_D,           KC_F,           KC_G,           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        RCTL_T(KC_QUOT),
-        KC_LGUI,         KC_Z,          KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_NO,
-        LT(2,KC_TAB),    RSFT_T(KC_SPC),LT(1,KC_ESC),  LT(1,KC_ENT),  LSFT_T(KC_BSPC),LT(2,KC_DEL)
-    ),
+    [_QWERTY] =  LAYOUT(KC_LGUI, LT(3,KC_GRV), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, LT(3,KC_LBRC), RALT_T(KC_RBRC), KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, RCTL_T(KC_QUOT), KC_LALT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO, LT(2,KC_TAB), RSFT_T(KC_SPC), LT(1,KC_ENT), LT(1,KC_ESC), LSFT_T(KC_BSPC), LT(2,KC_DEL)),
 
-    [_LOWER] = LAYOUT(
-        KC_TRNS,         KC_UNDS,       MS_BTN3,        MS_WHLL,        MS_WHLD,        MS_WHLU,        MS_WHLR,        MS_LEFT,        MS_DOWN,        MS_UP,          MS_RGHT,        MS_BTN1,         MS_BTN2,         RGUI_T(KC_F12),
-        LCTL_T(KC_PPLS), KC_EXLM,       KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,        KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_RPRN,        RCTL_T(KC_MINS),
-        LALT_T(KC_EQL),  KC_1,          KC_2,           KC_3,           KC_4,           KC_5,           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_NO,
-        KC_TRNS,         KC_TRNS,       KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS
-    ),
+    [_LOWER] = LAYOUT(KC_TRNS, KC_UNDS, KC_MPRV, KC_VOLD, KC_MPLY, KC_VOLU, KC_MNXT, KC_F1, KC_F2, KC_F3, KC_F9, KC_F10, KC_F11, RGUI_T(KC_F12), LCTL_T(KC_PPLS), KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, RCTL_T(KC_MINS), LALT_T(KC_EQL), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-    [_RAISE] = LAYOUT(
-        KC_TRNS,         KC_NUM,        KC_PSLS,        KC_P7,          KC_P8,          KC_P9,          KC_PMNS,        KC_MPRV,        KC_VOLD,        KC_VOLU,        KC_MNXT,        SS_ITEM,         KC_PSCR,         KC_TRNS,
-        LCTL_T(KC_PEQL), KC_PAST,       KC_P4,          KC_P5,          KC_P6,          KC_PPLS,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RGHT,        SS_MASTER,        RCTL_T(KC_APP),
-        KC_TRNS,         KC_P0,         KC_P1,          KC_P2,          KC_P3,          KC_PCMM,        KC_HOME,        KC_PGDN,        KC_PGUP,        KC_END,         KC_BSLS,        KC_NO,
-        KC_TRNS,         KC_TRNS,       KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS
-    ),
+    [_RAISE] = LAYOUT(KC_TRNS, KC_CAPS, KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_LCBR, KC_LBRC, SS_MASTER, SS_ITEM, KC_NO, KC_PSCR, KC_TRNS, LCTL_T(KC_PEQL), KC_PAST, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, RCTL_T(KC_APP), KC_TRNS, KC_P0, KC_P1, KC_P2, KC_P3, KC_PCMM, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_BSLS, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-    [_ADJUST] = LAYOUT(
-        QK_BOOT,         KC_NO,         AS_UP,          AS_TOGG,        AS_DOWN,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        AS_DOWN,        AS_TOGG,        AS_UP,           KC_NO,           QK_BOOT,
-        UG_TOGG,         UG_HUEU,       UG_SATU,        UG_VALU,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        UG_VALU,        UG_SATU,        UG_HUEU,        UG_TOGG,
-        UG_NEXT,         UG_HUED,       UG_SATD,        UG_VALD,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        UG_VALD,        UG_SATD,        UG_HUED,        UG_NEXT,
-        KC_TRNS,         SH_TOGG,       KC_TRNS,        KC_TRNS,        SH_TOGG,        KC_TRNS
-    )
+    [_ADJUST] = LAYOUT(KC_TRNS, KC_NO, KC_NO, KC_NO, MS_UP, MS_BTN2, MS_ACL2, MS_ACL2, MS_BTN3, MS_WHLU, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, MS_LEFT, MS_BTN1, MS_RGHT, MS_ACL1, MS_ACL1, MS_LEFT, MS_BTN1, MS_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, MS_DOWN, KC_NO, MS_ACL0, MS_ACL0, KC_NO, MS_DOWN, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -72,7 +52,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case SS_MASTER:
             if (record->event.pressed) {
-                SEND_STRING("0\n4547\n");
+                SEND_STRING("0\t4547\n");
             }
             return false;
 
